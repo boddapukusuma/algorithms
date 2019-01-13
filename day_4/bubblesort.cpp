ea99628bd12856e8880826ef29a386a2342f48eb
boddapukusuma <34337870+boddapukusuma@users.noreply.github.com>
2019-01-13 21:15:58 +0530

    Update bubblesort.cpp

--- day_4/bubblesort.cpp
+++ day_4/bubblesort.cpp
@@ -1,13 +1,14 @@
+//In bubble sort after completion  of each iteration we get the smallest element at the end(i.e, n-i-1).
 #include<stdio.h>
 #include<stdlib.h>
 void bubble_sort(int a[],int size)
 {
-	int i, j, temp;
+	int i, j, temp;  //temp is the temporary element
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (a[j]>a[j + 1])
+			if (a[j]>a[j + 1])      //if a[j]>a[j+1] swap the elements
 			{
 				temp = a[j];
 				a[j] = a[j + 1];
@@ -30,7 +31,7 @@ void main()
 	for (i = 0; i < size; i++)     //reading elements into array 
 		scanf("%d", &arr[i]);
 	bubble_sort(arr, size);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)    //printing elements
 		printf("%d ", arr[i]);
 	system("pause");
 }
